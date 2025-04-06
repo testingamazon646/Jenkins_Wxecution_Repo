fd66e6fa1fa7a2ef5118d55b43f889b276fb8941
HP <testingamazon646@gmail.com>
2025-04-06 11:28:40 +0530

    First Commit

---
+++ JenkinsExecutions/src/test/java/DemoPackage/Jankinstest.java
@@ -0,0 +1,16 @@
+package DemoPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Jankinstest {
+	@Test
+	public void demo() {
+		Reporter.log("Jainkins Test Pass",true);
+	}
+
+}
